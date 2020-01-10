7d962fd9cb4294699cdd38fc9f32ae6eb3bb57a3
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-01-11 03:21:20 +0600

    reducer for posts and users

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
+  renderPostList = () => {
+    return this.props.posts.map(post => (
+      <div className='item' key={post.id}>
+        <i className='large middle aligned icon user'></i>
+        <div className='content'>
+          <div className='description'>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+          <UserHeader userId={post.userId} />
+        </div>
+      </div>
+    ));
+  };
+
   render() {
-    return <div>PostList</div>;
+    return (
+      <div className='ui relaxed divided list'>{this.renderPostList()}</div>
+    );
   }
 }
 
-// const mapStateToProps = state => {
-//   return {
-//     posts:
-//   }
-// }
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
 
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-  goAway: () => 'Go away'
+  posts: postsReducer,
+  users: usersReducer
 });
